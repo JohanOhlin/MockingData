82a44e5ff1746ee57e10bf01480c473faa9c8eb1
Johan Ohlin <johan.ohlin@outlook.com>
2016-09-14 07:53:04 +0100

    Tests for streets

--- src/MockingDataTests/LocationData/When_Working_With_Streets.cs
+++ src/MockingDataTests/LocationData/When_Working_With_Streets.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
+using MockingData;
+using MockingData.Generators.Random;
 using MockingData.LocationData;
 using Xunit;
 
@@ -15,6 +17,38 @@ namespace MockingDataTests.LocationData
         public void All_Streets_Should_Have_A_Convertible_Postcode_Pattern()
         {
             // Each street should have a postal code pattern that can be converted without errors
+
+            // Arrange
+            var countries = Countries.GetValidRegisteredCountries();
+            var md = new MockingDataGenerator();
+            var pat = new PatternMatching(md.RandomGenerator);
+
+            // Act
+            var allCities = countries
+                .SelectMany(x => x.States)
+                .SelectMany(s => s.Cities);
+
+            var citiesWithStreetsWithInvalidPostalCode = new List<string>();
+            foreach (var city in allCities)
+            {
+                // We already have a check to verify that each city has streets so we don't need to do that here.
+                if (city.Streets == null) continue;
+
+                foreach (var street in city.Streets)
+                {
+                    try
+                    {
+                        pat.RandomAlphaNumFromPattern(street.PostalCode);
+                    }
+                    catch (InvalidPatternException)
+                    {
+                        citiesWithStreetsWithInvalidPostalCode.Add($"{city.Name} ({street.Name} - {street.PostalCode})");
+                    }
+                }
+            }
+
+            // Assert
+            citiesWithStreetsWithInvalidPostalCode.Should().HaveCount(0, $" all streets should have a VALID POSTAL CODE PATTERN (these cities have streets with invalid patterns: {string.Join(",", citiesWithStreetsWithInvalidPostalCode)})");
         }
 
         [Fact]
